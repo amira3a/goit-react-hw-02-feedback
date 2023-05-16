05a8ab37c28fd4ab2a7dfb901392ce796aceedc9
Amira <105366233+amira3a@users.noreply.github.com>
2023-05-16 12:08:16 +0300

    react-hw-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import FeedbackWidget from "./feedback/FeedbackWidget";
 export const App = () => {
   return (
     <div
@@ -10,7 +11,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <FeedbackWidget/>
     </div>
   );
 };
---
+++ src/components/feedback/FeedbackWidget.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import "./style.module.css"; // import the CSS file
+
+const Section = ({ title, children }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    return total ? Math.round((good / total) * 100) : 0;
+  };
+
+  return (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {countTotalFeedback()}</p>
+      <p>Positive feedback: {countPositiveFeedbackPercentage()}%</p>
+    </div>
+  );
+};
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map((option) => (
+        <button key={option} onClick={() => onLeaveFeedback(option)}>
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+const Notification = ({ message }) => {
+  return <p>{message}</p>;
+};
+
+const FeedbackWidget = () => {
+  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const handleFeedback = (type) => {
+    setState({ ...state, [type]: state[type] + 1 });
+  };
+
+  const feedbackOptions = ["good", "neutral", "bad"];
+
+  return (
+    <div className="feedback-widget">
+      <Section title="Give feedback">
+        <FeedbackOptions
+          options={feedbackOptions}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {state.good || state.neutral || state.bad ? (
+          <Statistics good={state.good} neutral={state.neutral} bad={state.bad} />
+        ) : (
+          <Notification message="No feedback given yet" />
+        )}
+      </Section>
+    </div>
+  );
+};
+
+export default FeedbackWidget;
